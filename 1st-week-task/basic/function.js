dc77d3ec2a896b808ca0ebe30db4321545d11ffc
Tjdnjs <2seowon@naver.com>
2022-10-04 20:42:14 +0900

    [update]

---
+++ 1st-week-task/basic/function.js
@@ -0,0 +1,17 @@
+// 함수 선언식
+
+function hello(name){
+    console.log(`안녕 ${name}`);
+}
+
+//함수 표현식  (화살표 함수)
+
+const sum = (a, b) => {
+    result = a + b;
+    console.log(result);
+}
+
+const sum_ = (a,b) => a+b; // 위랑 같은 함수
+
+sum(1,2)
+hello("박서원")
---
+++ 1st-week-task/basic/scope.js
@@ -0,0 +1,11 @@
+if (true) {
+    var x = "var"
+}
+
+console.log("x: ", x); // x: var
+
+if(true){
+    let y = "let"
+}
+
+console.log("y: ", y); // referenceError: y is not defined
