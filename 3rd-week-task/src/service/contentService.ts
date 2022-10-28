d1a3fd425bbd30d8de4b4a20e94a37eac9943b2c
Tjdnjs <2seowon@naver.com>
2022-10-28 11:42:57 +0900

    [update] Deduplication in contentService

--- 3rd-week-task/src/service/contentService.ts
+++ 3rd-week-task/src/service/contentService.ts
@@ -1,12 +1,13 @@
+const contents = require('./../data/content.json');
 
 const getContent = async (contentId: string) => {
-        const contents = require('./../data/content.json');
-        return contents[contentId];
+    return contents[contentId];
 }
+
 const getEpisode = async (contentId: string, episodeId:string) => {
-    const contents = require('./../data/content.json');
     return contents[contentId][episodeId];
 }
+
 export {
     getContent,
     getEpisode
